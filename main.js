b3e6f4bde253c33cb84128337291947f081ee428
Elizeu Farias <theadventuresdejonh_10@hotmail.com>
2024-05-08 20:18:51 -0400

    Inicio
    Commit inicial.

---
+++ main.js
@@ -0,0 +1,30 @@
+const form = document.getElementById('form-deposito');
+
+function validaNome(nomeCompleto) {
+    const nomeComoArray = nomeCompleto.split (' ');
+    return nomeComoArray.length >= 2;
+}
+
+form.addEventListener('submit', function(e) {
+    let formEValido = false;
+    e.preventDefault();
+
+    const nomeBeneficiario = document.getElementById('nome-beneficiario');
+    const numeroContaBeneficiario = document.getElementById('numero-conta');
+    const valorDeposito = document.getElementById('valor-deposito');
+    const mensagemSucesso = `Montante de: R$ <b>${valorDeposito.value}</b> foi depositado para o cliente: <b>${nomeBeneficiario.value}</b> na conta de nº <b>${numeroContaBeneficiario.value}</b>`;
+
+    formEValido = validaNome(nomeBeneficiario.value)
+    if (formEValido) {
+        document.querySelector('.success-message').innerHTML = mensagemSucesso;
+
+        nomeBeneficiario.value = '';
+        numeroContaBeneficiario.value = '';
+        valorDeposito.value = '';
+
+    } else {
+        alert("O nome não está completo!");
+    }
+})
+
+console.log(form);
